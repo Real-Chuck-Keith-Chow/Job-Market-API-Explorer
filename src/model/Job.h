0eda8eb913d8bbfa67cd5e8e38668466e3160e9b
Cheuk Fung Keith (Chuck) Chow <rosarollins069@gmail.com>
2026-05-03 21:18:04 -0400

    Small cleanup

--- src/model/Job.h
+++ src/model/Job.h
@@ -4,30 +4,31 @@
 #include <string>
 #include <vector>
 
-struct Location {
-    std::string display_name;
-    std::string area;
-    std::string country;
-};
-
-struct Company {
-    std::string display_name;
-    std::string id;
-};
+#include "Company.h"
+#include "Location.h"
 
 struct Job {
     std::string id;
     std::string title;
+
     Company company;
     Location location;
+
     double salary_min;
     double salary_max;
+
     std::string description;
     std::string redirect_url;
     std::vector<std::string> technologies;
     std::string created;
 
-    Job() : salary_min(0), salary_max(0) {}
+    Job()
+        : salary_min(0.0),
+          salary_max(0.0) {}
+
+    bool isValid() const {
+        return !title.empty() && company.isValid();
+    }
 };
 
 #endif
